4a20fccf7d34e1271cd065ba5f63d5b6a9e7f9ce
xuanhn.vt <xuanhn.vt@DESKTOP-KOBS6K3>
2020-09-03 23:39:55 +0700

    Custom project Ordering.API

---
+++ src/xRestaurant/Services/Ordering/Ordering.API/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ordering.API.Data.Entities;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private static readonly double[] Prices = new[]
+        {
+            45.23, 45.67, 12.64
+        };
+
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Order
+            {
+                CreatedOnUtc = DateTime.Now.AddDays(index),
+                OrderNo = rng.Next(-20, 55),
+                Price = Convert.ToDecimal(Prices[rng.Next(Prices.Length)])
+            })
+            .ToArray();
+        }
+    }
+}
--- src/xRestaurant/Services/Ordering/Ordering.API/Data/Entities/Order.cs
+++ src/xRestaurant/Services/Ordering/Ordering.API/Data/Entities/Order.cs
@@ -7,5 +7,11 @@ namespace Ordering.API.Data.Entities
 {
     public class Order
     {
+        public Guid Id { get; set; }
+        public Guid BuyerId { get; set; }
+        public long OrderNo { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public DateTime UpdatedOnUtc { get; set; }
     }
 }
