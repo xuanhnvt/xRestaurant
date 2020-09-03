3d8040eab2219ec948b8a49d1603c896ae0a36da
xuanhn.vt <xuanhn.vt@DESKTOP-KOBS6K3>
2020-09-03 23:16:36 +0700

    Add new project Catalog.API

---
+++ src/xRestaurant/Services/Catalog/Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Catalog.API.Data.Entities;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                CreatedOnUtc = DateTime.Now.AddDays(index),
+                StockQuantity = rng.Next(-20, 55),
+                Name = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ src/xRestaurant/Services/Catalog/Catalog.API/Data/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public long ProductNo { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public decimal Price { get; set; }
+        public bool Published { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public DateTime UpdatedOnUtc { get; set; }
+    }
+}
