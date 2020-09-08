9c8493609117e2bbb491516cd0cdefec7c7ece10
xuanhn.vt <xuanhn.vt@DESKTOP-KOBS6K3>
2020-09-08 22:12:52 +0700

    Add new project xSystem.Core and upgrade EF Core

---
+++ src/xRestaurant/Core/xSystem.Core/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class DbContextOptionsBuilderExtensions
+    {
+        //public static DbContextOptionsBuilder UseSqlServer([NotNullAttribute] this DbContextOptionsBuilder optionsBuilder, [NotNullAttribute] string connectionString)
+        //{
+
+        //}
+    }
+}
