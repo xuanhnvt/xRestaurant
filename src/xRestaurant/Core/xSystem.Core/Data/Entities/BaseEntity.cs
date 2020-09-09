fb7a37b591c8fc12db0b720139fdf279f901594e
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-09 14:36:35 +0700

    Add files via upload

---
+++ src/xRestaurant/Core/xSystem.Core/Data/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xSystem.Core.Data.Entities
+{
+    /// <summary>
+    /// Base class for entities
+    /// </summary>
+    public abstract class BaseEntity: BaseEntityWithGenericId<Guid>
+    {
+
+    }
+}
