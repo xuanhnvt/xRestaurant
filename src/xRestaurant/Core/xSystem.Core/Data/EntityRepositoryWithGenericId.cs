52ccb399ced663e67ed11031c02685e83e68cf79
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-10 08:52:22 +0700

    Create EntityRepositoryWithGenericId.cs

---
+++ src/xRestaurant/Core/xSystem.Core/Data/EntityRepositoryWithGenericId.cs
@@ -0,0 +1,227 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using xSystem.Core.Data.Entities;
+
+namespace xSystem.Core.Data
+{
+    /// <summary>
+    /// Represents an entity repository with generic key
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TKey">Key type of entity</typeparam>
+    public partial class EntityRepositoryWithGenericId<TEntity, TKey> : IEntityRepositoryWithGenericId<TEntity, TKey> where TEntity : class, IEntityId<TKey>
+    {
+        #region Fields
+
+        private readonly DbContext _context;
+
+        private DbSet<TEntity> _entities;
+
+        #endregion
+
+        #region Ctor
+
+        public EntityRepositoryWithGenericId(DbContext context)
+        {
+            this._context = context;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Rollback of entity changes and return full error message
+        /// </summary>
+        /// <param name="exception">Exception</param>
+        /// <returns>Error message</returns>
+        protected string GetFullErrorTextAndRollbackEntityChanges(DbUpdateException exception)
+        {
+            //rollback entity changes
+            if (_context is DbContext dbContext)
+            {
+                var entries = dbContext.ChangeTracker.Entries()
+                    .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified).ToList();
+
+                entries.ForEach(entry => entry.State = EntityState.Unchanged);
+            }
+
+            _context.SaveChanges();
+            return exception.ToString();
+        }
+
+        #endregion
+
+        #region Asynchronous Methods
+
+        /// <summary>
+        /// Asynchronously get entity by identifier
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        public virtual async Task<TEntity> GetByIdAsync(TKey id)
+        {
+            return await Entities.FindAsync(id);
+        }
+
+        /// <summary>
+        /// Asynchronously insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual async Task InsertAsync(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            try
+            {
+                await Entities.AddAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously insert entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual async Task InsertAsync(IEnumerable<TEntity> entities)
+        {
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+
+            try
+            {
+                await Entities.AddRangeAsync(entities);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously update entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            try
+            {
+                Entities.Update(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously update entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual async Task UpdateAsync(IEnumerable<TEntity> entities)
+        {
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+
+            try
+            {
+                Entities.UpdateRange(entities);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            try
+            {
+                Entities.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously delete entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual async Task DeleteAsync(IEnumerable<TEntity> entities)
+        {
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+
+            try
+            {
+                Entities.RemoveRange(entities);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a table
+        /// </summary>
+        public virtual IQueryable<TEntity> Table => Entities;
+
+        /// <summary>
+        /// Gets a table with "no tracking" enabled (EF feature) Use it only when you load record(s) only for read-only operations
+        /// </summary>
+        public virtual IQueryable<TEntity> TableNoTracking => Entities.AsNoTracking();
+
+        /// <summary>
+        /// Gets an entity set
+        /// </summary>
+        protected virtual DbSet<TEntity> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                    _entities = _context.Set<TEntity>();
+
+                return _entities;
+            }
+        }
+
+        #endregion
+    }
+}
