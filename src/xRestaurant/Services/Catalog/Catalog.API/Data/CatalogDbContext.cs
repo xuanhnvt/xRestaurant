2e49f7ec9c734ac489b22782c8784130d938aeb8
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-10 10:21:58 +0700

    Update CatalogDbContext.cs

--- src/xRestaurant/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
+++ src/xRestaurant/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
@@ -1,16 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Catalog.API.Data.Entities;
+using xSystem.Core.Data;
 
 namespace Catalog.API.Data
 {
-    public class CatalogDbContext: DbContext
+    public class CatalogDbContext: DbContext, ICatalogDbContext
     {
         public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
         {
+
         }
 
         public DbSet<Product> Products { get; set; }
