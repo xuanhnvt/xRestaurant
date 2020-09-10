d3fdc823e8528e11129584b376bf13359ce369f3
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-10 08:53:26 +0700

    Update DbContextOptionsBuilderExtensions.cs

--- src/xRestaurant/Core/xSystem.Core/Extensions/DbContextOptionsBuilderExtensions.cs
+++ src/xRestaurant/Core/xSystem.Core/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using xSystem.Core.Data;
 
 namespace Microsoft.EntityFrameworkCore
 {
     public static class DbContextOptionsBuilderExtensions
     {
-        //public static DbContextOptionsBuilder UseSqlServer([NotNullAttribute] this DbContextOptionsBuilder optionsBuilder, [NotNullAttribute] string connectionString)
-        //{
-
-        //}
+        public static DbContextOptionsBuilder UseDataProvider([NotNullAttribute] this DbContextOptionsBuilder optionsBuilder, [NotNullAttribute] DataProvider dataProvider)
+        {
+            if (dataProvider.Provider.Equals("MSSQL"))
+                optionsBuilder.UseSqlServer(dataProvider.ConnectionString);
+            else if (dataProvider.Provider.Equals("SQLite"))
+                optionsBuilder.UseSqlite(dataProvider.ConnectionString);
+            return optionsBuilder;
+        }
     }
 }
