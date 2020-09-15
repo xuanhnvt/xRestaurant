5956b355725a85d0e33d04bbd5e9a3faaeac34f7
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-15 17:24:41 +0700

    Create CartDomainEventHandler.cs

---
+++ src/xRestaurant/Services/Shopping/Shopping.API/Application/DomainEventHandlers/CartDomainEventHandler.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CQRSlite.Events;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data.Entities;
+using Shopping.Domain.Events.Cart;
+using xSystem.Core.Data;
+
+namespace Shopping.API.Application.DomainEventHandlers
+{
+    public class CartDomainEventHandler : ICancellableEventHandler<CartCreatedDomainEvent>,
+        ICancellableEventHandler<CartItemAddedDomainEvent>
+    {
+        private readonly ILogger<CartDomainEventHandler> _logger;
+        private readonly IEntityRepository<Cart> _repository;
+        //private readonly IEntityRepository<CartItem> _cartItemrepository;
+
+        public CartDomainEventHandler(ILogger<CartDomainEventHandler> logger, IEntityRepository<Cart> repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public async Task Handle(CartCreatedDomainEvent message, CancellationToken token)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var cart = new Cart()
+            {
+                Id = message.Id,
+                CartNo = 1,
+                Price = 0,
+                ShopperName = message.ShopperName,
+                ShopperId = message.ShopperId,
+                CreatedOnUtc = message.TimeStamp.UtcDateTime,
+                UpdatedOnUtc = message.TimeStamp.UtcDateTime
+            };
+            await _repository.InsertAsync(cart);
+        }
+
+        public async Task Handle(CartItemAddedDomainEvent message, CancellationToken token)
+        {
+            // get cart
+            var cart = await _repository.GetByIdAsync(message.Id);
+            var cartItem = new CartItem()
+            {
+                Id = message.CartItemId,
+                CartId = cart.Id,
+                ProductName = message.ProductName,
+                UnitPrice = message.UnitPrice,
+                Quantity = message.Quantity,
+                CreatedOnUtc = message.TimeStamp.UtcDateTime,
+                UpdatedOnUtc = message.TimeStamp.UtcDateTime
+            };
+            cart.CartItems.Add(cartItem);
+            await _repository.UpdateAsync(cart);
+        }
+    }
+}
