5bf7bc206a1c4215931c58253e3ad3dafe502deb
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-15 17:16:23 +0700

    Add files via upload

---
+++ src/xRestaurant/Services/Shopping/Shopping.API/Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ShoppingDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
