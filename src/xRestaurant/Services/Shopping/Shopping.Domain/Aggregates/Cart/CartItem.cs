41b7738525a9e7b2862e05190ded279b05130493
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-15 17:13:40 +0700

    Add files via upload

---
+++ src/xRestaurant/Services/Shopping/Shopping.Domain/Aggregates/Cart/CartItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping.Domain.Aggregates.Cart
+{
+    public class CartItem
+    {
+        public Guid Id { get; private set; }
+        public string ProductName { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+
+        public CartItem(Guid productId, string productName, decimal unitPrice, int quantity = 1)
+        {
+            if (quantity <= 0)
+            {
+                throw new ArgumentException("Invalid quantity number");
+            }
+
+            Id = productId;
+            ProductName = productName;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+        }
+
+        public void SetQuatity(int quantity)
+        {
+            Quantity = quantity;
+        }
+    }
+}
