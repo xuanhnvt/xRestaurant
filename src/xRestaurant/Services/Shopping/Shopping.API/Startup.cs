710e6c98608af89bf6bbd0dc6a5b3d36c2e5efa0
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-15 17:19:46 +0700

    Update Startup.cs

--- src/xRestaurant/Services/Shopping/Shopping.API/Startup.cs
+++ src/xRestaurant/Services/Shopping/Shopping.API/Startup.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using CQRSlite.Caching;
+using CQRSlite.Commands;
+using CQRSlite.Domain;
+using CQRSlite.Events;
+using CQRSlite.Messages;
+using CQRSlite.Queries;
+using CQRSlite.Routing;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Shopping.API.Application.CommandHandlers;
+using Shopping.API.Data;
+using xSystem.Core.Data;
 
 namespace Shopping.API
 {
@@ -25,6 +37,40 @@ namespace Shopping.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var catalogDataProvider = Configuration.GetSection("Data:Catalog").Get<DataProvider>();
+            services.AddDbContext<ShoppingDbContext>(options => options.UseDataProvider(catalogDataProvider));
+            services.AddScoped<IDbContext, ShoppingDbContext>();
+            services.AddScoped<IShoppingDbContext, ShoppingDbContext>();
+            services.AddScoped(typeof(IEntityRepository<>), typeof(EntityRepository<>));
+            services.AddScoped(typeof(IEntityRepositoryWithGenericId<,>), typeof(EntityRepositoryWithGenericId<,>));
+            services.AddMemoryCache();
+
+            //Add Cqrs services
+            services.AddSingleton<Router>(new Router());
+            services.AddSingleton<ICommandSender>(y => y.GetService<Router>());
+            services.AddSingleton<IEventPublisher>(y => y.GetService<Router>());
+            services.AddSingleton<IHandlerRegistrar>(y => y.GetService<Router>());
+            services.AddSingleton<IQueryProcessor>(y => y.GetService<Router>());
+            services.AddSingleton<IEventStore, InMemoryEventStore>();
+            services.AddSingleton<ICache, MemoryCache>();
+            services.AddScoped<IRepository>(y => new CacheRepository(new Repository(y.GetService<IEventStore>()), y.GetService<IEventStore>(), y.GetService<ICache>()));
+            services.AddScoped<ISession, Session>();
+
+            //Scan for commandhandlers and eventhandlers
+            services.Scan(scan => scan
+                .FromAssemblies(typeof(CartCommandHandler).GetTypeInfo().Assembly)
+                    .AddClasses(classes => classes.Where(x => {
+                        var allInterfaces = x.GetInterfaces();
+                        return
+                            allInterfaces.Any(y => y.GetTypeInfo().IsGenericType && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IHandler<>)) ||
+                            allInterfaces.Any(y => y.GetTypeInfo().IsGenericType && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(ICancellableHandler<>)) ||
+                            allInterfaces.Any(y => y.GetTypeInfo().IsGenericType && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IQueryHandler<,>)) ||
+                            allInterfaces.Any(y => y.GetTypeInfo().IsGenericType && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(ICancellableQueryHandler<,>));
+                    }))
+                    .AsSelf()
+                    .WithTransientLifetime()
+            );
+
             services.AddControllers();
         }
 
