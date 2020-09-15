6b1141391e1785ddd1111812caf6fc8fd2dc967e
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-15 17:15:09 +0700

    Update Cart.cs

--- src/xRestaurant/Services/Shopping/Shopping.API/Data/Entities/Cart.cs
+++ src/xRestaurant/Services/Shopping/Shopping.API/Data/Entities/Cart.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Collections.Generic;
+using xSystem.Core.Data.Entities;
 
 namespace Shopping.API.Data.Entities
 {
-    public class Cart
+    public class Cart: BaseEntity
     {
-        public Guid Id { get; set; }
         public Guid ShopperId { get; set; }
+        public string ShopperName { get; set; }
         public long CartNo { get; set; }
         public decimal Price { get; set; }
         public DateTime CreatedOnUtc { get; set; }
         public DateTime UpdatedOnUtc { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
     }
 }
