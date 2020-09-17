802b67c14abec175436701daf3384c6cd4aaf6a3
xuanhnvt <xuanhn.vt@gmail.com>
2020-09-17 17:22:54 +0700

    Create CartItemUpdatedDomainEvent.cs

---
+++ src/xRestaurant/Services/Shopping/Shopping.Domain/Events/Cart/CartItemUpdatedDomainEvent.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CQRSlite.Events;
+
+namespace Shopping.Domain.Events.Cart
+{
+    public class CartItemUpdatedDomainEvent : BaseEvent
+    {
+        public Guid CartItemId { get; }
+        public Guid ProductId { get; }
+        public string ProductName { get; }
+        public int Quantity { get; }
+        public decimal UnitPrice { get; }
+
+        public CartItemUpdatedDomainEvent(Guid cartId, Guid cartItemId, Guid productId, string productName, decimal unitPrice, int quantity)
+        {
+            Id = cartId;
+            CartItemId = cartItemId;
+            ProductId = productId;
+            ProductName = productName;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+    }
+}
